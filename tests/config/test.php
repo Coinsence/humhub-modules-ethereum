565cf21bff3dcdfd2a16ff79050b419b43b8a25e
craxrev <mortadha.ghanmi56@gmail.com>
2020-02-05 18:55:30 +0100

    use xcoin fixtures

--- tests/config/test.php
+++ tests/config/test.php
@@ -3,6 +3,7 @@
 return [
     'fixtures' => [
         'default',
+        'account' => 'humhub\modules\xcoin\tests\codeception\fixtures\AccountFixture'
     ],
     'humhub_root' => '/var/www/html'
 ];
